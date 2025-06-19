10d34e75c486fb26ef3f079243100913349ed59c
Diego Flores <diego.flores.guerra@gmail.com>
2025-06-18 21:54:48 -0500

    Cambios para proyecto karate inicial

---
+++ src/test/java/karate-config.js
@@ -0,0 +1,8 @@
+function fn() {
+  var config = {};
+  // Configuración global de Karate
+  config.baseUrl = 'http://localhost:8080';
+  // Puedes agregar más variables globales aquí
+  return config;
+}
+
